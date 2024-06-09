25ac84a842abfe8822b6fb39e35cb573b032e33e
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-06-09 10:06:36 +0530

    basic user api done

--- src/main/java/com/krishnavamshi/konnect/controllers/UserController.java
+++ src/main/java/com/krishnavamshi/konnect/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.krishnavamshi.konnect.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import javax.swing.text.html.Option;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -34,41 +37,58 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> getUsers() {
-        List<User> users = new ArrayList<>();
-        User u1 = new User(1, "Abcd", "Singh", "abcd@mail.com", "123456");
-        User u2 = new User(2, "Efgh", "Sharma", "efgh@mail.com", "654321");
-        users.add(u1);
-        users.add(u2);
+        // List<User> users = new ArrayList<>();
+        // User u1 = new User(1, "Abcd", "Singh", "abcd@mail.com", "123456");
+        // User u2 = new User(2, "Efgh", "Sharma", "efgh@mail.com", "654321");
+        // users.add(u1);
+        // users.add(u2);
+        // return users;
+        List<User> users = userRepository.findAll();
         return users;
     }
 
     @GetMapping("/users/{userId}")
-    public User getUserById(@PathVariable("userId") Integer id) {
-        User u1 = new User(1, "Xyz", "Khan", "xyz@mail.com", "123456");
-        u1.setId(id);
-        return u1;
+    public User getUserById(@PathVariable("userId") Integer id) throws Exception {
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()) {
+            return user.get();
+        }
+        throw new Exception("User Do not Exist with ID "+id);
     }
 
-    @PutMapping("/users")
-    public User updateUser(@RequestBody User user) {
-        User u1 = new User(1, "Xyz", "Khan", "xyz@mail.com", "123456");
+    @PutMapping("/users/{userId}")
+    public User updateUser(@RequestBody User user, @PathVariable("userId") Integer id) throws Exception {
+        Optional<User> u = userRepository.findById(id);
+        if(u.isEmpty()) {
+            throw new Exception("User Do not Exist with ID "+id);
+        }
+        User existedUser = u.get();
         if(user.getFirstName()!=null) {
-            u1.setFirstName(user.getFirstName());
+            existedUser.setFirstName(user.getFirstName());
         }
         if(user.getLastName()!=null) {
-            u1.setLastName(user.getLastName());
+            existedUser.setLastName(user.getLastName());
         }
         if(user.getEmail()!=null) {
-            u1.setEmail(user.getEmail());
+            existedUser.setEmail(user.getEmail());
         }
         if(user.getPassword()!=null) {
-            u1.setEmail(user.getEmail());
+            existedUser.setPassword(user.getPassword());
         }
-        return u1;
+
+        User updatedUser = userRepository.save(existedUser);
+        return updatedUser;
     }
 
     @DeleteMapping("/users/{userId}")
-    public String deleteUser(@PathVariable Integer userId) {   // (@PathVariable("userId") integer id)
-        return "User Deleted Successfully of ID "+userId;
+    public String deleteUser(@PathVariable Integer userId) throws Exception {   // (@PathVariable("userId") integer id)
+        Optional<User> existedUser = userRepository.findById(userId);
+        if(existedUser.isEmpty()) {
+            throw new Exception("User with ID "+userId+" Does not Exists");
+        }
+        // userRepository.delete(existedUser.get());
+        // OR
+        userRepository.deleteById(userId);
+        return "User Deleted with ID "+userId;
     }
 }
