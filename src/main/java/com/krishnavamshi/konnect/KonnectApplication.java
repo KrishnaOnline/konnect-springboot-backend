f01dd41677ee3bcbf8c1f56007316ec37c0b04fa
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-06-14 15:02:39 +0530

    implementing JWT Auth

--- src/main/java/com/krishnavamshi/konnect/KonnectApplication.java
+++ src/main/java/com/krishnavamshi/konnect/KonnectApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KonnectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KonnectApplication.class, args);
 	}
-
 }
--- src/main/java/com/krishnavamshi/konnect/config/AppConfig.java
+++ src/main/java/com/krishnavamshi/konnect/config/AppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -23,7 +24,7 @@ public class AppConfig {
                                   .authenticated()
                                   .anyRequest()
                                   .permitAll()
-        ).httpBasic().and()
+        ).addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
         .csrf(csrf -> csrf.disable());
         return http.build();
     }
