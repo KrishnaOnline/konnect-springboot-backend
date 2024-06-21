79855b4a878bb039f1da9051fe700b5495e4b992
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-06-21 11:36:31 +0530

    Exception Handling

---
+++ src/main/java/com/krishnavamshi/konnect/exceptions/UserException.java
@@ -0,0 +1,9 @@
+// package com.krishnavamshi.konnect.exceptions;
+
+// // Can create such Exceptions for each Model or any, as import then and use as throws UserException
+// // and is configured in GlobalExceptions class
+// public class UserException extends Exception {
+//     public UserException(String message) {
+//         super(message);
+//     }
+// }
