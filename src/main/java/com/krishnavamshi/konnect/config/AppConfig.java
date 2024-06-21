b9baa675a489c860e6403a8dcc537aa8be652986
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-06-21 11:15:37 +0530

    CORS configured

--- src/main/java/com/krishnavamshi/konnect/config/AppConfig.java
+++ src/main/java/com/krishnavamshi/konnect/config/AppConfig.java
@@ -1,7 +1,11 @@
 package com.krishnavamshi.konnect.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -9,6 +13,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
@@ -27,8 +35,32 @@ public class AppConfig {
                                   .anyRequest()
                                   .permitAll()
         ).addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
-        .csrf(csrf -> csrf.disable());
+        .csrf(csrf -> csrf.disable())
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+        
         return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+
+            @Override
+            // @Nullable
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(Arrays.asList(
+                    "http://localhost:5173/"
+                ));
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setExposedHeaders(Arrays.asList(
+                    "Authorization"
+                ));
+                cfg.setMaxAge(3600L);
+                return cfg;
+            }
+        };
     }
 
     @Bean
