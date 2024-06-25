0c67450db717235033ec0abb4418691dcd815044
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-06-25 20:29:33 +0530

    WebSockets in Spring boot

---
+++ src/main/java/com/krishnavamshi/konnect/controllers/RealtimeChatController.java
@@ -0,0 +1,20 @@
+package com.krishnavamshi.konnect.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+import com.krishnavamshi.konnect.models.Message;
+
+public class RealtimeChatController {
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat/{groupId}")
+    public Message sendToUser(@Payload Message message, @DestinationVariable String groupId) {
+        simpMessagingTemplate.convertAndSendToUser(groupId, "/private", message);
+        return message;
+    }
+}
